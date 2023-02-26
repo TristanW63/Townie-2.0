39a20079f342c84d19748e88eedf804eec73c99e
Tristan Barron <tristan_wilson_63@yahoo.com>
2023-02-26 17:43:55 -0600

    test

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ import {
 } from "react-router-dom";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
