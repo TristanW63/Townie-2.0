8055e7a1d30e18719e956f4c540e832e827aa9ba
Tristan Barron <tristan_wilson_63@yahoo.com>
2023-02-26 17:36:25 -0600

    test

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/town-socialDB", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://Tristan:root@cluster0.vpdwdoq.mongodb.net/?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
