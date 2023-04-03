2a2d2f64fefc17997b945d997be2703f0f78b828
Tristan Barron <tristan_wilson_63@yahoo.com>
2023-04-03 16:48:07 -0500

    link update

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ import { IdContext } from "./components/Friends/AddFriend";
 
 //this needs to be changed to the deployed link when deployed to heroku or netlify "http://localhost:3001/graphql"
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
+  uri: "/graphql",
 });
 
 //this is the token that is used to authenticate the user
